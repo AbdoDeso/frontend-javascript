dbbd1abbf1cd259f51904c2da60354150cbb2f50
Abdullah Eldesoky <microstar747@gmail.com>
2025-05-22 11:57:49 +0300

    update main

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -3,16 +3,14 @@ import { Java } from "./subjects/Java"
 import { React } from "./subjects/React"
 import { Teacher } from "./subjects/Teacher"
 
-const cTeacher: Teacher = { firstname: "John", experienceTeachingC: 10 };
+
+export const cTeacher: Teacher = { firstname: "Ahmed", experienceTeachingC: 10 };
 
 const cpp = new Cpp();
-cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements(), cpp.getAvailableTeacher());
 
 const react = new React();
-react.setTeacher(cTeacher);
 console.log(react.getRequirements(), react.getAvailableTeacher());
 
 const java = new Java();
-java.setTeacher(cTeacher);
 console.log(java.getRequirements(), java.getAvailableTeacher());
